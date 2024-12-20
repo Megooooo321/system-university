5721579626c83526201431d0f7d46316d74ac272
Megooooo321 <162889962+Megooooo321@users.noreply.github.com>
2024-12-20 16:06:09 +0200

    Add files via upload

---
+++ sytem university.cpp
@@ -0,0 +1,15 @@
+﻿'system university.exe' (Win32): Loaded 'C:\Users\p&p\source\repos\system university\x64\Debug\system university.exe'. Symbols loaded.
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\ntdll.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\kernel32.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\KernelBase.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\msvcp140d.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140d.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140_1d.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\ucrtbased.dll'. 
+The thread 13812 has exited with code 0 (0x0).
+The thread 26400 has exited with code 0 (0x0).
+The thread 21576 has exited with code 0 (0x0).
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\kernel.appcore.dll'. 
+'system university.exe' (Win32): Loaded 'C:\Windows\System32\msvcrt.dll'. 
+The thread 37952 has exited with code 0 (0x0).
+The program '[22460] system university.exe' has exited with code 0 (0x0).
